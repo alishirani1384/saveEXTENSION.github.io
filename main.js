851bb74872da7a9d65b80b978919956be70fd2d1
Ali Shirani <87601568+alishirani1384@users.noreply.github.com>
2021-10-03 13:03:39 -0700

    main.js
    this the js file.

--- main.js
+++ main.js
@@ -5,25 +5,17 @@ const ulel=document.getElementById('ul-el')
 const deletebtn=document.getElementById("del-btn")
 const leadsfromlocalstorage=JSON.parse(localStorage.getItem("myleads")) 
 const tabbtn=document.getElementById("tab-btn")
-
-    
-
-
 if(leadsfromlocalstorage){
     myleads=leadsfromlocalstorage
     render(myleads)
 }
-
 tabbtn.addEventListener("click",function () {
     chrome.tabs.query({active:true, currentWindow:true,},function (tabs) {
       myleads.push(tabs[0].url)
       localStorage.setItem("myleads", JSON.stringify(myleads))  
       render(myleads)     
-    })
-      
+    })    
 })
-
-
 function render (leads) {
 let listItems=""
 for (let i = 0; i < leads.length; i++) {
@@ -34,20 +26,14 @@ listItems+=`
 }
 ulel.innerHTML=listItems
 }
-
 deletebtn.addEventListener("dblclick",function () {
     localStorage.clear()
     myleads=[]
-    render(myleads)
-    
+    render(myleads) 
 })
-
-
-
 inputbtn.addEventListener('click',function () {
     myleads.push(inputel.value)
     inputel.value=""  
     localStorage.setItem("myleads", JSON.stringify(myleads))  
-    render(myleads)
-    
+    render(myleads) 
 })
